98eb4831416e0ccfbfcc8a2b27970465ceb0e0f9
Hanpa <twilight5703@gmail.com>
2019-12-26 16:45:39 +0900

    added output "Tell her world"

--- hello.py
+++ hello.py
@@ -1,8 +1,4 @@
-import os
-path = "C:/Users/Lee/git_tutorial"
-
-os.access(path, os.F_OK & os.R_OK & os.W_OK & os.E_OK)
-
-
 print ("Hello world")
 print ("Tell Your world")
+print ("Tell his world")
+print ("Tell her world")
